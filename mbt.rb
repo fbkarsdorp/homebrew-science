f8a83390f063e5f22ab842310ff27f294b4f9a3e
Folgert Karsdorp <fbkarsdorp@gmail.com>
2014-12-15 17:05:52 +0100

    mbt (add make check)

--- mbt.rb
+++ mbt.rb
@@ -9,24 +9,14 @@ class Mbt < Formula
   depends_on "pkg-config" => :build
   depends_on "timblserver"
 
+  option "without-check", "skip build-time checks (not recommended)"
+
   def install
     system "./configure", "--disable-debug",
                           "--disable-dependency-tracking",
                           "--disable-silent-rules",
                           "--prefix=#{prefix}"
+    system "make", "check" if build.with? "check"
     system "make", "install"
-  end
-
-  test do
-    # `test do` will create, run in and delete a temporary directory.
-    #
-    # This test will fail and we won't accept that! It's enough to just replace
-    # "false" with the main program this formula installs, but it'd be nice if you
-    # were more thorough. Run the test with `brew test mbt`. Options passed
-    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
-    #
-    # The installed folder is not in the path, so use the entire path to any
-    # executables being tested: `system "#{bin}/program", "do", "something"`.
-    system "false"
   end
 end
