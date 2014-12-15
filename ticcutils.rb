19dd4ee110035f22f92a9800e8df18300b57bf75
Folgert Karsdorp <fbkarsdorp@gmail.com>
2014-12-15 14:39:23 +0100

    ticcutils (latest) (new formula)
    ticcutils (add check)
    
    ticcutils (fail with clang)
    
    ticcutils (needs openmp)

---
+++ ticcutils.rb
@@ -0,0 +1,22 @@
+require "formula"
+
+class Ticcutils < Formula
+  homepage "http://ilk.uvt.nl/ticcutils/"
+  url "http://software.ticc.uvt.nl/ticcutils-0.7.tar.gz"
+  sha1 "f58ec1a6b64a0eebc1a8a74ef625cf6398a5fafc"
+
+
+  depends_on "pkg-config" => :build
+  needs :openmp
+
+  option "without-check", "skip build-time checks (not recommended)"
+
+  def install
+    system "./configure", "--disable-dependency-tracking",
+                          "--disable-silent-rules",
+                          "--prefix=#{prefix}"
+    system "make", "check" if build.with? "check"
+    system "make", "install"
+  end
+
+end
