525e956297aba004361392a236dd00f42b951770
Folgert Karsdorp <fbkarsdorp@gmail.com>
2014-12-15 17:08:17 +0100

    frogdata (add make check)

--- frogdata.rb
+++ frogdata.rb
@@ -8,24 +8,14 @@ class Frogdata < Formula
   depends_on "pkg-config" => :build
   depends_on "frog"
 
+  option "without-check", "skip build-time checks (not recommended)"
+
   def install
     system "./configure", "--disable-debug",
                           "--disable-dependency-tracking",
                           "--disable-silent-rules",
                           "--prefix=/usr/local/Cellar/frog/latest/"
+    system "make", "check" if build.with? "check"
     system "make", "install"
-  end
-
-  test do
-    # `test do` will create, run in and delete a temporary directory.
-    #
-    # This test will fail and we won't accept that! It's enough to just replace
-    # "false" with the main program this formula installs, but it'd be nice if you
-    # were more thorough. Run the test with `brew test frogdata`. Options passed
-    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
-    #
-    # The installed folder is not in the path, so use the entire path to any
-    # executables being tested: `system "#{bin}/program", "do", "something"`.
-    system "false"
   end
 end
